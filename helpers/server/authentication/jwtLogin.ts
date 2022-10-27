ca975a338c1e6dca37aa20f797e77fa23f98d634
Emilia Penney <elijahpenney@outlook.com>
2022-10-26 19:22:29 -0700

    Fixed JWT expiration

--- helpers/server/authentication/jwtLogin.ts
+++ helpers/server/authentication/jwtLogin.ts
@@ -4,7 +4,7 @@ import { TokenData } from '$types';
 
 export const jwtLogin = (data: TokenData) => {
   if (process.env.JWT_SECRET_KEY) {
-    return jwt.sign(data, process.env.JWT_SECRET_KEY);
+    return jwt.sign(data, process.env.JWT_SECRET_KEY, { expiresIn: parseInt(process.env.AUTH_TIMEOUT_MINUTES) * 60 });
   }
 
   throw new ApolloError('Attempted to sign JWT but JWT_SECRET_KEY was not defined');
